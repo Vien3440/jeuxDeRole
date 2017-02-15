84ea2ff533659674abb9ca8ad47e4b60b8f267f6
fflachet <flachet.f@gmail.com>
2017-02-15 16:49:33 +0100

    fix bugs repartition des points

--- web/js/stats.js
+++ web/js/stats.js
@@ -11,9 +11,13 @@ $(document).ready(function () {
 
 
     function pvUpdate($pv) {
-       
-        $('input[id=appbundle_stats_pv]').click(function () {
-             $rd =  parseInt($('input[id=rdStats]').val());
+        // empeche l'utilisation du clavier
+        $('input[id=appbundle_stats_pv]').keypress(function (evt) {
+            evt.preventDefault();
+        });
+
+        $('input[id=appbundle_stats_pv]').on("input propertychange", function () {
+            $rd = parseInt($('input[id=rdStats]').val());
             // si les pv sont modifié 
             if ($pv < $('input[id=appbundle_stats_pv]').val()) {
                 // je crée une variable avec la nouvelle valeur de rd 
@@ -27,16 +31,16 @@ $(document).ready(function () {
                 $('#appbundle_stats_pv').attr({
                     "max": $newPv + $rd
                 });
-            }else if ($pv > $('input[id=appbundle_stats_pv]').val()) {
-                
-                $rd =  $rd + 1; //Set new valeur
+            } else if ($pv > $('input[id=appbundle_stats_pv]').val()) {
+
+                $rd = $rd + 1; //Set new valeur
                 $('input[id=rdStats]').val($rd);//Get new valeur
                 // je crée une variable avec la nouvelle valeur de pv 
                 $newPv = parseInt($('input[id=appbundle_stats_pv]').val());
                 // j'affecte cette nouvelle valeur a pv 
                 $pv = $newPv;
 
-                
+
             }
 
 
@@ -45,8 +49,13 @@ $(document).ready(function () {
     }
 
     function attUpdate($att) {
-        $('input[id=appbundle_stats_att]').click(function () {
-                    var $rd = parseInt($('input[id=rdStats]').val());
+        // empeche l'utilisation du clavier
+        $('input[id=appbundle_stats_att]').keypress(function (evt) {
+            evt.preventDefault();
+        });
+        
+        $('input[id=appbundle_stats_att]').on("input propertychange", function () {
+            var $rd = parseInt($('input[id=rdStats]').val());
 
             // si les pv sont modifié 
             if ($att < $('input[id=appbundle_stats_att]').val()) {
@@ -62,16 +71,16 @@ $(document).ready(function () {
                 $('#appbundle_stats_att').attr({
                     "max": $newAtt + $rd
                 });
-            }else if ($att > $('input[id=appbundle_stats_att]').val()) {
-                
-                $rd =  $rd + 1; //Set new valeur
+            } else if ($att > $('input[id=appbundle_stats_att]').val()) {
+
+                $rd = $rd + 1; //Set new valeur
                 $('input[id=rdStats]').val($rd);//Get new valeur
                 // je crée une variable avec la nouvelle valeur de att
                 $newAtt = parseInt($('input[id=appbundle_stats_att]').val());
                 // j'affecte cette nouvelle valeur a att 
                 $att = $newAtt;
 
-                
+
             }
 
         });
